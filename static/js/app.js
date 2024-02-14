0c3b77feb83fb010f1c833d909a66143feaf417b
Ajay S. Nawalkar <109715598+Ajaynawalkar@users.noreply.github.com>
2024-02-14 18:15:54 +0530

    employee management system based on CRUD operation

---
+++ static/js/app.js
@@ -0,0 +1,11 @@
+
+//Message or Notification Timer
+var message_timeout = document.getElementById("message-timer");
+
+setTimeout(function()
+
+{
+
+    message_timeout.style.display="none";
+
+}, 3000);
